90b28ae8de29b98c1e583afadcd16c5324f2f779
Kousuke Takeuchi <u651601f@gmail.com>
2021-04-14 23:36:30 +0900

    odometryの算出まで実装

--- tello_driver/tello_driver/tello_driver.py
+++ tello_driver/tello_driver/tello_driver.py
@@ -11,8 +11,9 @@ from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 
 from cv_bridge import CvBridge
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, Imu
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 from tello_msgs.msg import TelloState
 from tello_msgs.srv import TelloAction
 
@@ -20,38 +21,84 @@ from tello_msgs.srv import TelloAction
 class TelloDriver(Node):
     # skip first 300 frames
     frame_skip = 300
+    vel_cmd_scale = 10
 
     def __init__(self):
         super().__init__('tello_driver')
         
         self.drone = tellopy.Tello()
-        self.drone.subscribe(self.drone.EVENT_FLIGHT_DATA, self.flight_data_handler)
+        self.drone.subscribe(self.drone.EVENT_LOG_DATA, self.flight_data_handler)
 
         self.drone.connect()
         self.drone.wait_for_connection(60.0)
 
         # self.container = av.open(self.drone.get_video_stream())
         
-        self.cmd_vel_sub_ = self.create_subscription(Twist,
+        self._cmd_vel_sub = self.create_subscription(Twist,
                                                     "cmd_vel",
                                                     self.cmd_vel_callback,
                                                     qos_profile_sensor_data)
         self.bridge = CvBridge()
-        self.image_pub_ = self.create_publisher(Image, "image_raw", 1)
-        self.state_pub_ = self.create_publisher(TelloState, "tello/state", 1)
-        self.action_srv_ = self.create_service(TelloAction, 'tello/action', self.action_callback)
+        self._image_pub = self.create_publisher(Image, "image_raw", 1)
+        self._imu_pub = self.create_publisher(Imu, "imu", 1)
+        self._odom_pub = self.create_publisher(Odometry, "odom", 1)
+        self._state_pub = self.create_publisher(TelloState, "tello/state", 1)
+        self._action_srv = self.create_service(TelloAction, 'tello/action', self.action_callback)
 
         timer_period = 0.01  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
        
     def flight_data_handler(self, event, sender, data, **args):
-        if event is self.drone.EVENT_FLIGHT_DATA:
-            pass
+        current_time = self.get_clock().now().to_msg()
+
+        odom_msg = Odometry()
+        odom_msg.header.stamp = current_time
+        odom_msg.header.frame_id = 'base_link'
+
+        # Height from MVO received as negative distance to floor
+        odom_msg.pose.pose.position.z = -data.mvo.pos_z #self.height #-data.mvo.pos_z
+        odom_msg.pose.pose.position.x = data.mvo.pos_x
+        odom_msg.pose.pose.position.y = data.mvo.pos_y
+        odom_msg.pose.pose.orientation.w = data.imu.q0
+        odom_msg.pose.pose.orientation.x = data.imu.q1
+        odom_msg.pose.pose.orientation.y = data.imu.q2
+        odom_msg.pose.pose.orientation.z = data.imu.q3
+        # Linear speeds from MVO received in dm/sec
+        odom_msg.twist.twist.linear.x = data.mvo.vel_y/10
+        odom_msg.twist.twist.linear.y = data.mvo.vel_x/10
+        odom_msg.twist.twist.linear.z = -data.mvo.vel_z/10
+        odom_msg.twist.twist.angular.x = data.imu.gyro_x
+        odom_msg.twist.twist.angular.y = data.imu.gyro_y
+        odom_msg.twist.twist.angular.z = data.imu.gyro_z
+                
+        self._odom_pub.publish(odom_msg)
+        
+        imu_msg = Imu()
+        imu_msg.header.frame_id = 'base_link'
+        imu_msg.header.stamp = current_time
+        
+        imu_msg.orientation.w = data.imu.q0
+        imu_msg.orientation.x = data.imu.q1
+        imu_msg.orientation.y = data.imu.q2
+        imu_msg.orientation.z = data.imu.q3        
+        imu_msg.angular_velocity.x = data.imu.gyro_x
+        imu_msg.angular_velocity.y = data.imu.gyro_y
+        imu_msg.angular_velocity.z = data.imu.gyro_z
+        imu_msg.linear_acceleration.x = data.imu.acc_x
+        imu_msg.linear_acceleration.y = data.imu.acc_y
+        imu_msg.linear_acceleration.z = data.imu.acc_z
+        
+        self._imu_pub.publish(imu_msg)
+        
+    def __scale_vel_cmd(self, cmd_val):
+        return self.vel_cmd_scale * cmd_val
 
     def cmd_vel_callback(self, cmd_vel):
-        self.robot.go(robot_dir*FWD_SPEED,robot_rot*ROT_SPEED)
-        # self.cmd_vel_pub_.publish(cmd_vel)
-
+        self.drone.set_pitch( self.drone.__scale_vel_cmd(cmd_vel.linear.y) )
+        self.drone.set_roll( self.drone.__scale_vel_cmd(cmd_vel.linear.x) )
+        self.drone.set_yaw( self.drone.__scale_vel_cmd(cmd_vel.angular.z) )
+        self.drone.set_throttle( self.drone.__scale_vel_cmd(cmd_vel.linear.z) )
+        
     def action_callback(self, command, response):
         # Arbitrary Tello command:
         # string cmd
@@ -65,12 +112,26 @@ class TelloDriver(Node):
         if command.cmd == 'takeoff':
             self.drone.takeoff()
             response.rc = response.OK
+        elif command.cmd == 'throw_and_go':
+            self.drone.throw_and_go()
+            response.rc = response.OK
         elif command.cmd == 'land':
             self.drone.land()
             response.rc = response.OK
-        elif command.cmd == 'flip':
-            self.drone.clockwise()
+        elif command.cmd == 'flip_forward':
+            self.drone.flip_forward()
             response.rc = response.OK
+        elif command.cmd == 'flip_back':
+            self.drone.flip_back()
+            response.rc = response.OK
+        elif command.cmd == 'flip_left':
+            self.drone.flip_left()
+            response.rc = response.OK
+        elif command.cmd == 'flip_right':
+            self.drone.flip_right()
+            response.rc = response.OK
+        else:
+            response.rc = response.ERROR_UNKNOWN
         return response
 
     def timer_callback(self):
@@ -78,14 +139,12 @@ class TelloDriver(Node):
         # self.get_image_stream()
 
     def get_image_stream(self):
-        print("hello")
-        print(self.frame_skip)
         for frame in self.container.decode(video=0):
             if 0 < self.frame_skip:
                 self.frame_skip = self.frame_skip - 1
                 continue
             image = self.bridge.cv2_to_imgmsg(np.array(frame.to_image()), "bgr8")
-            self.image_pub_.publish(image)
+            self._image_pub.publish(image)
 
     def on_destroy(self):
         self.drone.quit()
